3d6a48922e9487aa53d3ae444bdc9f39fb239d9e
bryce-88 <yengpeng.khor@gmail.com>
2023-01-27 13:32:01 +0800

    completed task02

--- task02/src/sdf/Main.java
+++ task02/src/sdf/Main.java
@@ -1,7 +1,6 @@
 package sdf;
 
 import java.io.Console;
-import java.util.Scanner;
 
 public class Main {
 
@@ -10,14 +9,57 @@ public class Main {
         Console cons = System.console();
         
         String input;
-        float result;
-        boolean exit = false;
+        float $last = 0f; //this is also result of operation
+        String[] arr = null; 
+        String operation;
+        float firstVal = 0f;
+        float secondVal = 0f;
 
-        while(!exit) {
-            input = cons.readLine("Welcome\n"); //assume user will type input in correct format after this
-            System.out.printf("> %s", input);
 
+        input = cons.readLine("Welcome\n"); //assume user will type input in correct format after this
+        while(true) {
+            System.out.printf("> %s\n", input);
+            arr = input.trim().split(" ");
+            operation = arr[1];
 
+            //user types exit
+            if (arr.length < 2 && arr[0].equals("exit")) {
+                System.out.println("Bye Bye");
+                System.exit(1);
+            } 
+            
+            if ((!arr[0].equals("$last"))) {
+                firstVal = Float.parseFloat(arr[0]);
+            } 
+
+            if (!arr[2].equals("$last")) {
+                secondVal = Float.parseFloat(arr[2]);
+            }
+            
+            if (arr[0].equals("$last")) {
+                firstVal = $last;
+            } 
+
+            if (arr[2].equals("$last")) {
+                secondVal = $last;
+            }
+
+            switch (operation) {
+                case Constants.PLUS: $last = firstVal + secondVal;
+                break;
+                case Constants.MINUS: $last = firstVal - secondVal;
+                break;
+                case Constants.DIVIDE: $last = firstVal / secondVal;
+                break;
+                case Constants.MULTIPLY: $last = firstVal * secondVal;
+                break;
+                default: 
+            }
+            
+
+            System.out.printf("%.2f\n", $last);
+            input = cons.readLine();
         }
+        
     }
 }
